7008f07ed722cc2104208a390793e84fb4e2d586
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-02 02:16:15 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,12 @@
-// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-// 引入 home 模組程式碼
 const home = require('./modules/home')
-
+const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 // 將網址結構符合，字串的 request 導向 home 模組
 router.use('/', home)
-
-// 引入 todos 模組程式碼
-const restaurants = require('./modules/restaurants')
 // 將網址結構符合todos字串開頭的 request 導向 todos 模組
 router.use('/restaurants', restaurants)
-
 // 匯出路由器
+router.use('/users', users)
 module.exports = router
